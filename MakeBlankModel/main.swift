4eebfe70a33e26b677c9b2114beb40b064aece24
BuildTools <unconfigured@null.spigotmc.org>
2022-12-18 17:56:24 -0500

    Fixed training issue

--- MakeBlankModel/main.swift
+++ MakeBlankModel/main.swift
@@ -24,6 +24,7 @@ func prepareModel() -> Model {
     var newModel = Model(version: 4, author: "William Hahn", license: "MIT")
     
     newModel.addInput(Input(name: "data_in", shape: [1]))
+    newModel.addTrainingInput(TrainingInput(name: "training_data_in", shape: [1]))
     newModel.addOutput(Output(name: "data_out", shape: [1]))
     
     newModel.neuralNetwork.addLayer(InnerProduct(name: "ip1", input: ["data_in"], output: ["ip1_out"], inputChannels: 1, outputChannels: 8, updatable: true))
