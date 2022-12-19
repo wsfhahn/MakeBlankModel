58ecb1dce2b437684a631292a5f647d8d7e37f27
BuildTools <unconfigured@null.spigotmc.org>
2022-12-19 09:08:53 -0500

    made it better

--- MakeBlankModel/main.swift
+++ MakeBlankModel/main.swift
@@ -21,15 +21,36 @@ func makeModelURL() -> URL {
 }
 
 func prepareModel() -> Model {
-    var newModel = Model(version: 4, author: "William Hahn", license: "MIT")
+    var newModel = Model(version: 4, author: "William Hahn", license: "MIT") {
+        NeuralNetwork(losses: [CategoricalCrossEntropy(name: "lossLayer",
+                                   input: "data_out",
+                                   target: "output_true")],
+                      optimizer: Adam(learningRateDefault: 0.0001,
+                                     learningRateMax: 0.3,
+                                     miniBatchSizeDefault: 8,
+                                     miniBatchSizeRange: [8],
+                                     beta1Default: 0.9,
+                                     beta1Max: 1.0,
+                                     beta2Default: 0.999,
+                                     beta2Max: 1.0,
+                                     epsDefault: 0.00000001,
+                                     epsMax: 0.00000001),
+                      epochDefault: UInt(5),
+                      epochSet: [UInt(5)],
+                      shuffle: true)
+    }
     
     newModel.addInput(Input(name: "data_in", shape: [1]))
-    newModel.addTrainingInput(TrainingInput(name: "training_data_in", shape: [1]))
+    
+    newModel.addTrainingInput(TrainingInput(name: "data_in", shape: [1]))
+    newModel.addTrainingInput(TrainingInput(name: "output_true", shape: [1]))
+    
     newModel.addOutput(Output(name: "data_out", shape: [1]))
     
     newModel.neuralNetwork.addLayer(InnerProduct(name: "ip1", input: ["data_in"], output: ["ip1_out"], inputChannels: 1, outputChannels: 8, updatable: true))
     newModel.neuralNetwork.addLayer(InnerProduct(name: "ip2", input: ["ip1_out"], output: ["ip2_out"], inputChannels: 8, outputChannels: 8, updatable: true))
-    newModel.neuralNetwork.addLayer(InnerProduct(name: "ip3", input: ["ip2_out"], output: ["data_out"], inputChannels: 8, outputChannels: 1, updatable: true))
+    newModel.neuralNetwork.addLayer(InnerProduct(name: "ip3", input: ["ip2_out"], output: ["ip3_out"], inputChannels: 8, outputChannels: 1, updatable: true))
+    newModel.neuralNetwork.addLayer(Softmax(name: "softmax", input: ["ip3_out"], output: ["data_out"]))
     
     return newModel
 }
